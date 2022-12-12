cc444ab704a677e1abcb6155ca1f106d58d6758c
mjanjic75 <mjanjic75@gmail.com>
2022-12-12 20:45:57 +0100

    Dodat EventListener za DOMContentLoaded i napravljene "sitne" izmene ;)

--- js/myScript.js
+++ js/myScript.js
@@ -1,19 +1,25 @@
+var charWarnning = 140; // kada ostane 5 karaktera, obojiti poruku crvenom bojom
+var txtAreaLimit = 150;
 
- document.getElementById('limitComment').addEventListener('input', txtCounter);
-
-var outputMessage = document.getElementById('output');
-var charWarnning = 15; // kada ostane 5 karaktera, obojiti poruku crvenom bojom
-var txtAreaLimit = 20;
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('limitComment').addEventListener('input', txtCounter);
+    document.getElementById('output').innerHTML = `Preostalo ${txtAreaLimit} karaktera!`;
+    document.forms[0].reset();
+});
 
 function txtCounter() {
+    var outputMessage = document.getElementById('output');
     var charCount = document.getElementById('limitComment').value.length;
     var txtComment = document.getElementById('limitComment');
 
     if (charCount > txtAreaLimit) {
         txtComment.value = txtComment.value.substring(0, txtAreaLimit);
+        charCount = txtAreaLimit;
+        // ako se "pejstuje" npr. 30 karaktera, biće inače prikazano
+        // da je preostalo -10, pa je potrebno postaviti vrednost na 0 u liniji 30
     }
-    
-    if (charCount - charWarnning > 0) {
+
+    if (charCount - charWarnning >= 0) {
         // outputMessage.classList.add('red');
         // outputMessage.classList.remove('black');
         outputMessage.classList.add('text-danger');
